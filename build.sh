653dd4b2ebbcab4aeb56ea4b4ad2f9c7d9762e72
Ahn, Ki Yung <kyagrd@gmail.com>
2019-07-19 12:04:07 +0900

    delete

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 ## run this inside docker
+
+DEPS=unbound-generics
+
 if [ -d "/opt/stack/snapshots" ]; then
 	if [ -d ".snapshots" ]; then
-		stack build
+		stack install $DEPS
 	else
-	       	mv /opt/stack/snapshots "$PWD/.snapshots" && ln -s "$PWD/.snapshots" /opt/stack/snapshots && stack build
+	       	mv /opt/stack/snapshots "$PWD/.snapshots" && ln -s "$PWD/.snapshots" /opt/stack/snapshots && stack install $DEPS
 	fi
 else
 	echo "Directory /opt/stack/snapshots does not exist."
