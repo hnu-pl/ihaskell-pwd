1a0f7e6908269b2359f928af02c8a46bd3773cee
Ahn, Ki Yung <kyagrd@gmail.com>
2019-07-19 22:34:24 +0900

    added a comment for those who want jupyter lab

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
+### uncomment below if you want Jupiter Lab 
+# LAB="--env JUPYTER_ENABLE_LAB=yes"
+
 if [ -z "$(ls -A ".snapshots" 2>/dev/null)" ]; then
 	docker run --rm -p 8888:8888 \
-		-v "$PWD":/home/jovyan/pwd \
+		-v "$PWD":/home/jovyan/pwd $LAB \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
 	       	crosscompass/ihaskell-notebook:latest
 else
 	docker run --rm -p 8888:8888 \
-		-v "$PWD":/home/jovyan/pwd -v "$PWD/.snapshots":/opt/stack/snapshots \
+		-v "$PWD":/home/jovyan/pwd $LAB \
+	       	-v "$PWD/.snapshots":/opt/stack/snapshots \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
 	       	crosscompass/ihaskell-notebook:latest
 fi
