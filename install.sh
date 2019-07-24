439e85289f0dead01945a9d90518ff420e9ae0e5
Ki Yung Ahn <kyagrd@gmail.com>
2019-07-24 16:27:57 +0900

    both global-project and snapshots copied to host

--- install.sh
+++ install.sh
@@ -5,10 +5,16 @@
 DEPS="unbound-generics tree-view"
 
 if [ -d "/opt/stack/snapshots" ]; then
-	if [ -d ".snapshots" ]; then
+	if [ -d ".snapshots" -a -d ".stack-work" ]; then
 		stack install $DEPS
 	else
-	       	mv /opt/stack/snapshots "$PWD/.snapshots" && ln -s "$PWD/.snapshots" /opt/stack/snapshots && stack install $DEPS
+		./clean.sh \
+			&& echo "copying snapshots and global-projct/.stack-work to host" \
+			&& mv /opt/stack/snapshots "$PWD/.snapshots" \
+			&& ln -s "$PWD/.snapshots" /opt/stack/snapshots \
+			&& mv /opt/stack/global-project/.stack-work "$PWD/.stack-work" \
+			&& ln -s "$PWD/.stack-work" /opt/stack/global-project/ \
+			&& stack install $DEPS
 	fi
 else
 	echo "Directory /opt/stack/snapshots does not exist."
--- run.sh
+++ run.sh
@@ -4,14 +4,15 @@
 HOSTPORT=8888
 
 if [ -z "$(ls -A ".snapshots" 2>/dev/null)" ]; then
-	docker run --rm -p $HOSTPORT:8888 \
-		-v "$PWD":/home/jovyan/pwd $LAB \
+	docker run --rm -p $HOSTPORT:8888 $LAB \
+		-v "$PWD":/home/jovyan/pwd \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
 	       	crosscompass/ihaskell-notebook:latest
 else
-	docker run --rm -p $HOSTPORT:8888 \
-		-v "$PWD":/home/jovyan/pwd $LAB \
+	docker run --rm -p $HOSTPORT:8888 $LAB \
+		-v "$PWD":/home/jovyan/pwd \
 	       	-v "$PWD/.snapshots":/opt/stack/snapshots \
+	       	-v "$PWD/.stack-work":/opt/stack/global-project/.stack-work \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
 	       	crosscompass/ihaskell-notebook:latest
 fi
