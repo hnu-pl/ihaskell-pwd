2bd8b8045566bd19d09a351a6a2fa9b108b1318d
Ki Yung Ahn <kyagrd@gmail.com>
2019-10-03 14:24:16 +0900

    fix docker image tag to specific digest

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
-### uncomment below if you want Jupiter Lab 
-# LAB="--env JUPYTER_ENABLE_LAB=yes"
-
+LAB="--env JUPYTER_ENABLE_LAB=yes"
+TAG=e763dc764d90
 HOSTPORT=8888
 
 if [ -z "$(ls -A ".snapshots" 2>/dev/null)" ]; then
 	docker run --rm -p $HOSTPORT:8888 $LAB \
 		-v "$PWD":/home/jovyan/pwd \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
-	       	crosscompass/ihaskell-notebook:latest
+	       	crosscompass/ihaskell-notebook:$TAG
 else
 	docker run --rm -p $HOSTPORT:8888 $LAB \
 		-v "$PWD":/home/jovyan/pwd \
 	       	-v "$PWD/.snapshots":/opt/stack/snapshots \
 	       	-v "$PWD/.stack-work":/opt/stack/global-project/.stack-work \
 		--env JUPYTER_TOKEN=x --name ihaskell_notebook \
-	       	crosscompass/ihaskell-notebook:latest
+	       	crosscompass/ihaskell-notebook:$TAG
 fi
